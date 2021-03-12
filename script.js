8402acc9a57b6aac262335a8ac536ff56db26072
Enaeemullah <35742914+Enaeemullah@users.noreply.github.com>
2021-03-12 15:58:13 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+var firstname= window.prompt("Enter Your First Name:");
+var lastname= window.prompt("Enter Your Second Name:");
+document.write(firstname + "  " + lastname);
+console.log(firstname + "  " + lastname);
+<br></br>
